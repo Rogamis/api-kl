385209e37d294d3e83b758148a2ed2b695d2da6b
Bogdan <roug.rogamis@gmail.com>
2022-07-18 20:39:37 +0300

    deploy

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import path from "path";
 import cors from "cors";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.static(path.resolve(path.resolve(), "static")));
 
